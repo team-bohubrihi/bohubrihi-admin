79d80a71c021d64a2cb17ecfd1306769291a8dbc
Shuvo <shuvo22890@gmail.com>
2021-07-02 10:33:47 +0600

    Working on combining sidebar & my word

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from '../../hoc/PrivateRoute';
+import { useSelector } from 'react-redux';
 import Auth from '../auth/Auth';
 import Recover from '../auth/Recover';
 import Courses from '../courses/Courses';
-import ManageCourse from '../courses/ManageCourse';
 import NotFound404 from '../404/404';
+import ManageCourse from '../courses/ManageCourse';
 import Header from '../header/Header';
 import Home from './home/Home';
+import Sidebar from '../../UIs/Sidebar';
+import '../../css/sidebar.css'
+const Body = () => {
+    const [sidebar, setsidebar] = React.useState(false);
+    const handlesidebar = () => setsidebar(!sidebar);
+    const token = useSelector((state) => state.auth.token);
 
-const Body = () => (<>
-    <Header />
-    <Switch>
-        <PrivateRoute exact path="/" component={Home} />
-        <PrivateRoute path="/courses" component={Courses} />
-        <PrivateRoute path="/course/:courseId" component={ManageCourse} />
+    return(
+        <>
+        <Header />
+        <div className="row">
+          {token ? (
+            <div className="col-sm-2">
+                <div className='mysidebartoggler' onClick={handlesidebar}>
+                    <i className="fas fa-bars"></i>
+                </div>
+                <div className={sidebar ? "nav-menu active" : "nav-menu"}>
+                    <Sidebar handlesidebar={handlesidebar}/>
+                </div>
+            </div>):null}
 
-        <Route path="/recover" component={Recover} />
-        <Route path="/login" component={Auth} />
-        <Route path='*' component={NotFound404} />
-    </Switch>
-</>);
+          <div className={token?"col-sm-10":"col-sm-12"}>
+            <Switch>
+                <PrivateRoute exact path="/" component={Home} />
+                <PrivateRoute path="/courses" component={Courses} />
+                <PrivateRoute path="/course/:courseId" component={ManageCourse} />
+
+                <Route path="/recover" component={Recover} />
+                <Route path="/login" component={Auth} />
+                <Route path='*' component={NotFound404} />
+            </Switch>
+          </div>
+        </div>
+        </>
+    );
+}
 export default Body;
